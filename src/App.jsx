8ddea3451761dddc8794b42df58dff3a8e725f77
Juan Kaman <juankaman1202@gmail.com>
2024-03-25 23:08:56 -0300

    criacao de components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import iconSedans from "./assets/icon-sedans.svg"
-import iconSuvs from "./assets/icon-suvs.svg"
-import iconLuxury from "./assets/icon-luxury.svg"
+import Luxuosos from "./components/Luxuosos"
+import Sedans from "./components/Sedans"
+import SUVs from "./components/Suvs"
+
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justif-center items-center">
       <section className="lg:flex max-w-{920px}">
-        <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
-          <img src={iconSedans} alt="icone carros sedans" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
-          <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-          <a className="font-lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange hover:text-offwhitebg" href="https://www.webmotors.com.br/sedans?idcmp=s08%3Ac13%3A3ea9720bc8b644d8a93f04b278d48332&gclsrc=aw.ds&gad_source=1&gclid=CjwKCAjwnv-vBhBdEiwABCYQA7No_ri1GEwaFE5--aZq3JWn3PhZwInsz9PqpAUAuqIlqtc08s0J9RoCyVIQAvD_BwE" target="_blank">Ver mais</a>
-        </div>
-        <div className="bg-green p-12">
-          <img src={iconSuvs} alt="icone Suvs" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">SUVs</h2>
-          <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-          <a className="font-lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg" href="https://www.webmotors.com.br/suvs?idcmp=s08%3Ac13%3Acc5557bdc18f4ebb8d2de994389066c7&gclsrc=aw.ds&gad_source=1&gclid=CjwKCAjwnv-vBhBdEiwABCYQAy--DNeGesAfPo1QpuJ0v7c80udQU9_vk5ht23qb3X1GQFkn84vfwhoCMmUQAvD_BwE" target="_blank">Ver mais</a>
-        </div>
-        <div className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
-          <img src={iconLuxury} alt="icon luxury" className="mb-7"/>
-          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
-          <p className="font-lexend mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a className="font-lexend bg-offwhitebg px-10 py-3 w-fit rounded-3xl text-dark-green border-2 border-offwhitebg hover:bg-dark-green hover:text-offwhitebg" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
-        </div>
+        <Sedans />
+        <SUVs />
+        <Luxuosos />
       </section>
     </main>
     )
